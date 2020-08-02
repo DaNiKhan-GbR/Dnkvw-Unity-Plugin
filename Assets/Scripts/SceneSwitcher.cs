268be04844bffbfb99dd9e5cae3c8d7fc5b36815
Daniel Klintworth <daniel.klintworth@gmail.com>
2020-08-03 01:42:41 +0200

    Added functionality to switch between scenes.

---
+++ Assets/Scripts/SceneSwitcher.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{    
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            if(SceneManager.GetActiveScene().name == "Room")
+                SceneManager.LoadScene(sceneName: "Sample Scene");
+            else
+                SceneManager.LoadScene(sceneName: "Room");
+        }
+    }
+}
